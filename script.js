3c1724c05b2e9a410e0e2e4c81658541c35e029a
ImPump <1076426280@qq.com>
2022-03-02 22:23:16 +0800

    create file
    新建文件

---
+++ script.js
@@ -0,0 +1,4 @@
+const top250 = 'https://api.wmdb.tv/api/v1/top?type=Imdb&skip=0&limit=250&lang=Cn';
+const searchApi = 'https://api.wmdb.tv/api/v1/movie/search?q=hello';
+const form = document.getElementById('form');
+const loading = document.querySelector('.loading-mask');
